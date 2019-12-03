421bff204cada7882434607de2aa48d1f5aae1a8
aliffarizky <58410206+aliffarizky@users.noreply.github.com>
2019-12-03 16:17:52 +0700

    Update part2.ino

--- part2.ino
+++ part2.ino
@@ -1,8 +1,8 @@
 #include <SoftwareSerial.h>
+#define DEBUG true
 SoftwareSerial myGsm(3,4);
 String data_csq[5];
-String data_ip[8];
-#define DEBUG true
+String data_ip[3];
 String csq,real_csq,ip;
 
 void setup()
@@ -13,22 +13,32 @@ void setup()
 
  Serial.println("======================INIT=============================");
  sendData("AT+CGATT=1",200,DEBUG); //enable GPRS Connection
+ delay(1000);
  sendData("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"",1000,DEBUG); //Declare Connection Type (GPRS)
+ delay(1000);
  sendData("AT+SAPBR=3,1,\"APN\",\"\"",5000,DEBUG); //Delare APN Name
+ delay(1000);
  sendData("AT+SAPBR=1,1",10000,DEBUG); //Enable GPRS & APN Setting
+ delay(1000);
  sendData("AT+CSQ",200,DEBUG); //GSM Signal Quality
  //Value from AT+CSQ 1 = -111 dBm with +2 dBm each incerement on value
+ delay(1000);
 
  sendData("AT+HTTPINIT",2000,DEBUG); //Initializing HTTP
+ delay(1000);
  sendData("AT+HTTPPARA=\"URL\",\"http://ipv4bot.whatismyipaddress.com\"",1000,DEBUG); //Setting HTTP Parameter
+ delay(1000);
  sendData("AT+HTTPACTION=0",3000,DEBUG); //Enabling GET Action
+ delay(1000);
  sendipData("AT+HTTPREAD=0,20",3000,DEBUG); //Read HTTP Data and parsing it
+ delay(1000);
  sendData("AT+HTTPTERM",1000,DEBUG); //Terminate HTTP Service
-
+ delay(1000);
  sendCSQData("AT+CSQ",200,DEBUG); //Parsing GSM signal response
+ delay(1000);
  Serial.println("================DONE, PRINT IP ADRESS & GSM STRENGTH======================");
  Serial.println("Ip:"+ip);
- real_csq = (((csq.toInt() - 1) * 2) - 111);
+ real_csq = (((csq.toInt() - 1) * 2) - 111);  //Value from AT+CSQ 1 = -111 dBm with +2 dBm each incerement on value
  Serial.println("Gsm:"+real_csq+" dBm");
  
 }
@@ -82,7 +92,7 @@ void sendCSQData(String command , const int timeout , boolean debug)
       {
         i++;  
       }
-      if (i == 6) 
+      if (i == 3) 
       {
         delay(100);
         goto exitL;
@@ -114,7 +124,7 @@ void sendipData(String command , const int timeout , boolean debug){
       } else {
         i++;  
       }
-      if (i == 8) {
+      if (i == 3) {
         delay(100);
         goto exitL;
       }
